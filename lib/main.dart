9a68e1d3782dfc5f7b0b3c4e7504c5779a4189ad
José Arturo Bustamante Lazcano <arturobl00@msn.com>
2021-02-03 17:58:33 -0600

    primer subida

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyDialog(),
+  ));
+}
+
+class MyDialog extends StatefulWidget {
+  @override
+  _MyDialogState createState() => _MyDialogState();
+}
+
+class _MyDialogState extends State<MyDialog> {
+  String textoInput = "";
+
+  void showAlert(String value) {
+    AlertDialog dialog = AlertDialog(
+      content: Text(value),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            precionaSi();
+          },
+          child: Text("Si"),
+        ),
+        FlatButton(
+          onPressed: () {
+            precionaNo();
+          },
+          child: Text("No"),
+        ),
+      ],
+    );
+
+    showDialog(context: context, child: dialog);
+  }
+
+  void cuandoCambie(String value) {
+    setState(() {
+      textoInput = value;
+    });
+  }
+
+  void precionaSi() {
+    print("Si");
+  }
+
+  void precionaNo() {
+    print("No");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Alert Dialog"),
+      ),
+      body: Container(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(hintText: "Ingrese Texto"),
+                  onChanged: (String value) {
+                    cuandoCambie(value);
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    child: Text("Ver Alerta"),
+                    onPressed: () {
+                      showAlert(textoInput);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
